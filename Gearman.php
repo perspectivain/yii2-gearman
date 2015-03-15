b00289a6faa456adc7e9ba5d4b3ff8c12b70cb71
Juliano Baggio di Sopra <jsopra@gmail.com>
2015-03-15 01:28:23 -0300

    Create Gearman.php

---
+++ Gearman.php
@@ -0,0 +1,43 @@
+<?php
+
+class Gearman extends CApplicationComponent
+{
+    public $servers;
+    protected $client;
+    protected $worker;
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    protected function setServers($instance)
+    {
+        foreach ($this->servers as $s) {
+            $instance->addServer($s['host'], $s['port']);
+        }
+
+        return $instance;
+    }
+
+    public function client()
+    {
+        if (!$this->client) {
+            $this->client = $this->setServers(new GearmanClient());
+        }
+
+        return $this->client;
+    }
+
+    public function worker()
+    {
+        if (!$this->worker) {
+            $this->worker = $this->setServers(new GearmanWorker());
+        }
+        
+        $this->worker->addOptions(GEARMAN_WORKER_GRAB_UNIQ);
+
+        return $this->worker;
+    }
+
+}
